4753a4bc4fb518bb195942a97c0d4da4e70348fc
amirah <mirez.data@gmail.com>
2024-11-24 11:07:25 +0800

    changes in tailwind since it's not work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
 
-function MainApp() {
+export default function App() {
   return (
-    <>
-    </>
+    <div className="bg-white">
+      <h1 className="text-3xl font-bold underline">
+      Hello World!
+      </h1>
+    </div>
   )
 }
 
-export default MainApp
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MainApp from './App'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MainApp />
+    <App />
   </StrictMode>,
 )
